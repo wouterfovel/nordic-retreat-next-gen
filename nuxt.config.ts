1ad7bacf2d5c4c09198b499f6bfd56fc2cabe289
Wouter Fovel <wouter.fovel@gmail.com>
2022-12-21 20:33:02 +0100

    feat: nuxt3 / tailwind / storyblok

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+import {defineNuxtConfig} from 'nuxt/config';
+
+export default defineNuxtConfig({
+  css: ['@/assets/css/roboto.css'],
+  modules: [
+    [
+      '@storyblok/nuxt',
+      {
+        accessToken: 'oJawF4KpCLNrdHws9Yqp9gtt',
+      },
+    ],
+    '@nuxtjs/tailwindcss',
+  ],
+})
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: "jit",
+  content: ['storyblok/**/*.{vue,js}', 'components/**/*.{vue,js}', 'pages/**/*.vue'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: 'Roboto, sans-serif',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
